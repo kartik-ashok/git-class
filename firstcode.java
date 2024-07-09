e7fd537ba4ed9879746bf256eaa21a7bfd7c7ada
karthik <kartikkatti55@gmail.com>
2024-07-09 23:41:43 +0530

    third commit

--- firstcode.java
+++ firstcode.java
@@ -1,7 +1,6 @@
 class FirstCode{
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello India");
+        System.out.println("Hello india this is important");
         for (int i = 0; i <10; i++) {
             System.out.println("Hey Media");
             
