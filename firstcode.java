0675b4d57925082d8a538b9488b45cdde254dfbb
karthik <kartikkatti55@gmail.com>
2024-07-10 00:36:22 +0530

    fifth commit

--- firstcode.java
+++ firstcode.java
@@ -1,9 +1,9 @@
 class FirstCode{
     public static void main(String[] args) {
-        System.out.println("Hello india this is important");
+        System.out.println("I am working in chromosis technology");
+        System.out.println("and this is main branch");
         for (int i = 0; i <10; i++) {
-            System.out.println("Hey namo");
-            
+            System.out.println(i+"*"+i+"="+i*i);
         }
 
     }
