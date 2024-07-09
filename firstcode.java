3bbe87216d20542eb412acc2e7bfafe9a5405a48
karthik <kartikkatti55@gmail.com>
2024-07-10 00:27:51 +0530

    main3

--- firstcode.java
+++ firstcode.java
@@ -3,7 +3,7 @@ class FirstCode{
         System.out.println("Hello world");
         System.out.println("Hello India");
         for (int i = 0; i <10; i++) {
-            System.out.println("Hey Media");
+            System.out.println("Hey namo");
             
         }
 
